79cafe667ad6344c737101525e53800eef587a43
Ruscher <rruscher@gmail.com>
2022-12-26 22:41:25 -0300

    Fix sort list kernel

--- usr/share/bigbashview/bcc/apps/drivers/script.js
+++ usr/share/bigbashview/bcc/apps/drivers/script.js
@@ -153,3 +153,28 @@ $(".pop-up3 .close").click(function () {
   $(".pop-up3").removeClass("visible");
 });
 
+
+sortList(document.getElementsByClassName('list')[0]);
+
+function sortList(ul){
+    var new_ul = ul.cloneNode(false);
+
+    // Add all lis to an array
+    var lis = [];
+    for(var i = ul.childNodes.length; i--;){
+        if(ul.childNodes[i].nodeName === 'LI')
+            lis.push(ul.childNodes[i]);
+    }
+
+    // Sort the lis in descending order
+    lis.sort(function(a, b){
+       return parseInt(b.childNodes[0].data , 10) - 
+              parseInt(a.childNodes[0].data , 10);
+    });
+
+    // Add them into the ul in order
+    for(var i = 0; i < lis.length; i++)
+        new_ul.appendChild(lis[i]);
+    ul.parentNode.replaceChild(new_ul, ul);
+}
+
